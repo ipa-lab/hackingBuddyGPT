650761b0a873b5369d109cc3a0fa20b0880a17d7
Andreas Happe <andreashappe@snikt.net>
2023-04-30 21:18:16 +0200

    ask GPT for an explanation after each command

--- wintermute.py
+++ wintermute.py
@@ -19,12 +19,13 @@ conn.connect()
 print("Get initial user from virtual machine:")
 initial_user = conn.run("whoami")
 
+sysinfo = "This is a linux-based system."
+
 while True:
 
-    next_cmd = create_and_ask_prompt('gpt_query.txt', "next-cmd", user=initial_user, history=cmd_history.get_history())
+    # TODO: separate between techniques (let GPT search for vulnerabiltiites) and procedures (concrete exploitation of a technique). This would make the exeuction loop a bit harder to understand and hierarchical, e.g., select a technique -> ask GPT how to exploit this technique (with a command sequence) -> execute and watch
 
-    # disable this for now, it's tragic because the AI won't tell me why it had chosen something
-    # create_and_ask_prompt("why.txt", "why", user=initial_user, history=cmd_history.dump(), next_cmd=next_cmd)
+    next_cmd = create_and_ask_prompt('query_next_command.txt', "next-cmd", user=initial_user, history=cmd_history.get_history())
 
     resp = conn.run(next_cmd)
     cmd_history.append(next_cmd, resp)
@@ -32,5 +33,9 @@ while True:
     # this will already by output by conn.run
     # logs.warning("server-output", resp)
 
+    # aks chatgpt to explain what it expects about the tested
+    # system. Understanding this might help human learning
+    system_explanation = create_and_ask_prompt('explain_system.txt', 'explain-system', sysinfo=sysinfo, cmd=next_cmd, cmd_output=resp)
+
     # this asks for additional vulnerabilities identifiable in the last command output
-    # create_and_ask_prompt('further_information.txt', 'vulns', user=initial_user, next_cmd=next_cmd, resp=resp)
+    # create_and_ask_prompt('query_vulnerabilities.txt', 'vulns', user=initial_user, next_cmd=next_cmd, resp=resp)

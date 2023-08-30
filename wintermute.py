29effbd7e1316c82fa25b1d6c1489f7a9142497e
Andreas Happe <andreashappe@snikt.net>
2023-08-30 12:00:41 +0200

    unify control flow and extract cmd handlers

--- wintermute.py
+++ wintermute.py
@@ -6,8 +6,6 @@ import paramiko
 from rich.console import Console
 from rich.panel import Panel
 
-from dotenv import load_dotenv
-
 from history import ResultHistory, num_tokens_from_string
 from targets.ssh import get_ssh_connection, SSHHostConn
 from llms.openai_rest import get_openai_response
@@ -15,31 +13,54 @@ from prompt_helper import LLM
 
 # setup some infrastructure
 cmd_history = ResultHistory()
+console = Console()
 
 # read configuration from env and configure system parts
 config.check_config()
 
+# open SSH connection to target
 conn = get_ssh_connection(config.target_ip(), config.target_user(), config.target_password())
 conn.connect()
 
+# TODO: why do I even have this code?
 print("Get initial user from virtual machine:")
 initial_user = conn.run("whoami")
 
+# initialize LLM connection
 llm = LLM(get_openai_response)
-
-sysinfo = "This is a linux-based system."
-
-console = Console()
 
 context_size = config.context_size()
 print("used model: " + config.model() + " context-size: " + str(config.context_size()))
+
+# prepare an initial state
+sysinfo = "This is a linux-based system."
 
 state = """
 - this is a linux system
 - your low privilege user credentials are lowpriv:trustno1
 """
 
-# maybe generate list of potential vulnerabilities based upon the system facts
+def handle_cmd(input):
+    return input["cmd"], conn.run(input["cmd"])
+
+def handle_ssh(input):
+    user = input["username"]
+    password = input["password"]
+
+    cmd = "tried ssh with username " + user + " and password " + password
+
+    test = SSHHostConn(config.target_ip(), user, password)
+    try:
+        test.connect()
+        user = conn.run("whoami")
+
+        if user == "root":
+            return cmd, "Login as root was successful"
+        else:
+            return cmd, "Authentication successful, but user is not root"
+
+    except paramiko.ssh_exception.AuthenticationException:
+        return cmd, "Authentication error, credentials are wrong"
 
 while True:
     # todo: take the list of potential vulnerabilities, go through each element and try up to 10 times to exploit the vulnerability
@@ -52,60 +73,31 @@ while True:
     diff = str(toc-tic)
 
     if next_cmd["type"]  == "cmd":
-        # run the command
-        cmd = next_cmd["cmd"]
-        resp = conn.run(cmd)
-        console.print(Panel(resp, title=cmd))
-
-        # analyze the result and update your state
-        resp_success = llm.create_and_ask_prompt('successfull.txt', 'success?', cmd=cmd, resp=resp, facts=state)
-
-        success = resp_success["success"]
-        reason = resp_success["reason"]
-
-        state = "\n".join(map(lambda x: "- " + x, resp_success["facts"]))
-        console.print(Panel(state, title="my new fact list"))
+        cmd, result = handle_cmd(next_cmd)
 
         # this asks for additional vulnerabilities identifiable in the last command output
         # next_vulns = create_and_ask_prompt('query_vulnerabilitites.txt', 'vulns', user=initial_user, next_cmd=cmd, resp=resp)
         # console.print(Panel(str(next_vulns), title="Next Vulns"))
 
     elif next_cmd["type"] == "ssh":
+        cmd, result = handle_ssh(next_cmd)
 
-        user = next_cmd["username"]
-        password = next_cmd["password"]
+    # output the command and it's result
+    console.print(Panel(result, title=cmd))
 
-        cmd = "tried ssh with username " + user + " and password " + password
+    # analyze the result and update your state
+    resp_success = llm.create_and_ask_prompt('successfull.txt', 'success?', cmd=cmd, resp=result, facts=state)
 
-        test = SSHHostConn(config.target_ip(), user, password)
-        authenticated = False
-        try:
-            test.connect()
-            authenticated = True
-        except paramiko.ssh_exception.AuthenticationException:
-            print("seems like SSH authentication failed..")
+    success = resp_success["success"]
+    reason = resp_success["reason"]
 
-        if not authenticated:
-            success = "False"
-            result = "Authentication error"
-            reason = "Login/Password was not correct"
-        else:
-            user = conn.run("whoami")
+    state = "\n".join(map(lambda x: "- " + x, resp_success["facts"]))
+    console.print(Panel(state, title="my new fact list"))
 
-            if user == "root":
-                success = "True"
-                result = "Authentication successful"
-                reason = "Login was successful"
-            else:
-                success = "False"
-                result = "Authentication success, but user is not root"
-                rason = "Login was successful but not root"
-
-    # update our command history
-    cmd_history.append(diff, next_cmd["type"], cmd, resp, success, reason)
+    # update our command history and output it
+    cmd_history.append(diff, next_cmd["type"], cmd, result, success, reason)
+    console.print(cmd_history.create_history_table())
 
     # aks chatgpt to explain what it expects about the tested
     # system. Understanding this might help human learning
     # system_explanation = create_and_ask_prompt('explain_system.txt', 'explain-system', sysinfo=sysinfo, cmd=next_cmd, cmd_output=resp)
-
-    console.print(cmd_history.create_history_table())

9c4f9a28cb576dcbcd8823555ddb4b5e35eef222
Andreas Happe <andreashappe@snikt.net>
2023-09-06 19:56:53 +0200

    add rough connector for oobabooga

---
+++ llms/oobabooga.py
@@ -0,0 +1,99 @@
+import html
+import json
+import re
+
+import requests
+
+from config import oobabooga_url
+
+# For local streaming, the websockets are hosted without ssl - http://
+url = oobabooga_url()
+URI = f'{HOST}/api/v1/chat'
+
+# For reverse-proxied streaming, the remote will likely host with ssl - https://
+# URI = 'https://your-uri-here.trycloudflare.com/api/v1/chat'
+
+
+def run(user_input, history):
+    request = {
+        'user_input': user_input,
+        'max_new_tokens': 250,
+        'auto_max_new_tokens': False,
+        'max_tokens_second': 0,
+        'history': history,
+        'mode': 'instruct',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+        'character': 'Example',
+        #'instruction_template': 'Vicuna-v1.1',  # Will get autodetected if unset
+        'your_name': 'You',
+        # 'name1': 'name of user', # Optional
+        # 'name2': 'name of character', # Optional
+        # 'context': 'character context', # Optional
+        # 'greeting': 'greeting', # Optional
+        # 'name1_instruct': 'You', # Optional
+        # 'name2_instruct': 'Assistant', # Optional
+        # 'context_instruct': 'context_instruct', # Optional
+        # 'turn_template': 'turn_template', # Optional
+        'regenerate': False,
+        '_continue': False,
+        'chat_instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
+
+        # Generation params. If 'preset' is set to different than 'None', the values
+        # in presets/preset-name.yaml are used instead of the individual numbers.
+        'preset': 'None',
+        'do_sample': True,
+        'temperature': 0.7,
+        'top_p': 0.1,
+        'typical_p': 1,
+        'epsilon_cutoff': 0,  # In units of 1e-4
+        'eta_cutoff': 0,  # In units of 1e-4
+        'tfs': 1,
+        'top_a': 0,
+        'repetition_penalty': 1.18,
+        'repetition_penalty_range': 0,
+        'top_k': 40,
+        'min_length': 0,
+        'no_repeat_ngram_size': 0,
+        'num_beams': 1,
+        'penalty_alpha': 0,
+        'length_penalty': 1,
+        'early_stopping': False,
+        'mirostat_mode': 0,
+        'mirostat_tau': 5,
+        'mirostat_eta': 0.1,
+        'guidance_scale': 1,
+        'negative_prompt': '',
+
+        'seed': -1,
+        'add_bos_token': True,
+        'truncation_length': 2048,
+        'ban_eos_token': False,
+        'skip_special_tokens': True,
+        'stopping_strings': []
+    }
+
+    response = requests.post(URI, json=request)
+
+    if response.status_code == 200:
+        return response.json()['results'][0]['history']
+
+
+def get_openai_response(cmd):
+
+    # Basic example
+    history = {'internal': [], 'visible': []}
+
+    # "Continue" example. Make sure to set '_continue' to True above
+    # arr = [user_input, 'Surely, here is']
+    # history = {'internal': [arr], 'visible': [arr]}
+
+    result = run(cmd, history)
+    data = json.dumps(html.unescape(result['visible'][-1][1]))
+    
+    pos = data.find('\\"cmd\\":')
+    if pos != -1:
+        print("this should be a command")
+        pos += len("\"cmd\":") + 5
+        tmp = data[pos:-6]
+        return str(json.dumps({ "type" : "cmd", "cmd" : tmp.replace("\\\"", "\"")}))
+
+    return html.unescape(result['visible'][-1][1])

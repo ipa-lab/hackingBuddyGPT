2441c0198b909105e0a6643c6546b3d8d58e274f
Andreas Happe <andreashappe@snikt.net>
2023-10-07 15:45:30 +0200

    cleanups: do cmd-cleanup earlier; unify token cutdown methods
    Also try to switch for another template for llama2

--- handlers.py
+++ handlers.py
@@ -5,9 +5,8 @@ import re
 from targets.ssh import SSHHostConn
 
 def handle_cmd(conn, input):
-    cmd = cmd_output_fixer(input)
-    result, gotRoot = conn.run(cmd)
-    return cmd, result, gotRoot
+    result, gotRoot = conn.run(input)
+    return input, result, gotRoot
 
 
 def handle_ssh(target, input):
@@ -30,36 +29,3 @@ def handle_ssh(target, input):
     except paramiko.ssh_exception.AuthenticationException:
         return input, "Authentication error, credentials are wrong\n", False
     
-
-def remove_wrapping_characters(cmd, wrappers):
-    if cmd[0] == cmd[-1] and cmd[0] in wrappers:
-        print("will remove a wrapper from: " + cmd)
-        return remove_wrapping_characters(cmd[1:-1], wrappers)
-    return cmd
-
-# often the LLM produces a wrapped command
-def cmd_output_fixer(cmd):
-
-    if len(cmd) < 2:
-        return cmd
-
-    stupidity = re.compile(r"^[ \n\r]*```.*\n(.*)\n```$", re.MULTILINE)
-    result = stupidity.search(cmd)
-    if result:
-        print("this would have been captured by the multi-line regex 1")
-        cmd = result.group(1)
-        print("new command: " + cmd)
-    stupidity = re.compile(r"^[ \n\r]*~~~.*\n(.*)\n~~~$", re.MULTILINE)
-    result = stupidity.search(cmd)
-    if result:
-        print("this would have been captured by the multi-line regex 2")
-        cmd = result.group(1)
-        print("new command: " + cmd)
-    stupidity = re.compile(r"^[ \n\r]*~~~.*\n(.*)\n~~~$", re.MULTILINE)
-
-    cmd = remove_wrapping_characters(cmd, "`'\"")
-
-    if cmd.startswith("$ "):
-        cmd = cmd[2:]
-    
-    return cmd
--- llm_with_state.py
+++ llm_with_state.py
@@ -5,6 +5,7 @@ import typing
 from db_storage import DbStorage
 from dataclasses import dataclass
 from mako.template import Template
+from cmd_cleaner import cmd_output_fixer
 
 @dataclass
 class LLMResult:
@@ -46,32 +47,39 @@ class LLMWithState:
         template_size = num_tokens_from_string(model, TPL_NEXT.source)
 
         history = get_cmd_history_v3(model, self.llm_connection.get_context_size(), self.run_id, self.db, state_size+template_size)
+        # history = ''
 
         if self.target.os == "linux":
             target_user = "root"
         else:
             target_user = "Administrator"
 
-        return self.create_and_ask_prompt_text(TPL_NEXT, history=history, state=self.state, target=self.target, update_state=self.enable_update_state, target_user=target_user)
+        cmd = self.create_and_ask_prompt_text(TPL_NEXT, history=history, state=self.state, target=self.target, update_state=self.enable_update_state, target_user=target_user)
+        cmd.result = cmd_output_fixer(cmd.result)
+        return cmd
 
     def analyze_result(self, cmd, result):
 
         model = self.llm_connection.get_model()
         ctx = self.llm_connection.get_context_size()
+        state_size = num_tokens_from_string(model, self.state)
+        target_size = ctx - SAFETY_MARGIN - state_size 
+
+        # ugly, but cut down result to fit context size
+        result = trim_result_front(model, target_size, result)
+        return self.create_and_ask_prompt_text(TPL_ANALYZE, cmd=cmd, resp=result, facts=self.state)
+
+    def update_state(self, cmd, result):
 
         # ugly, but cut down result to fit context size
         # don't do this linearly as this can take too long
-        CUTOFF_STEP = 128
-        current_size = num_tokens_from_string(model, result)
-        while current_size > (ctx + 512):
-            cut_off = int(((current_size - (ctx + 512)) + CUTOFF_STEP)/2)
-            result = result[cut_off:]
-            current_size = num_tokens_from_string(model, result)
+        model = self.llm_connection.get_model()
 
-        result = self.create_and_ask_prompt_text(TPL_ANALYZE, cmd=cmd, resp=result, facts=self.state)
-        return result
+        ctx = self.llm_connection.get_context_size()
+        state_size = num_tokens_from_string(model, self.state)
+        target_size = ctx - SAFETY_MARGIN - state_size
+        result = trim_result_front(model, target_size, result)
 
-    def update_state(self, cmd, result):
         result = self.create_and_ask_prompt_text(TPL_STATE, cmd=cmd, resp=result, facts=self.state)
         self.state = result.result
         return result
@@ -104,7 +112,7 @@ def num_tokens_from_string(model: str, string: str) -> int:
         encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     return len(encoding.encode(string))
 
-STEP_CUT_TOKENS : int = 32
+STEP_CUT_TOKENS : int = 128
 SAFETY_MARGIN : int = 128
 
 # create the command history. Initially create the full command history, then
@@ -120,21 +128,45 @@ def get_cmd_history_v3(model: str, ctx_size: int, run_id: int, db: DbStorage, to
         result = result + '$ ' + itm[0] + "\n" + itm[1]
 
     # trim it down if too large
-    cur_size = num_tokens_from_string(model, result) + token_overhead + SAFETY_MARGIN
-        
-    while cur_size > ctx_size:
-        diff = cur_size - ctx_size
-        step = int((diff + STEP_CUT_TOKENS)/2)
-        result = result[:-step]
-        cur_size = num_tokens_from_string(model, result) + token_overhead + SAFETY_MARGIN
+    cur_size = num_tokens_from_string(model, result)
     
-    return result
+    allowed = ctx_size - SAFETY_MARGIN - token_overhead
+    return trim_result_front(model, allowed, result)
 
 def wrap_it_for_llama(prompt):
-    return f"""### System:
-you are a concise but helful learning tool that aids students trying to find security vulnerabilities
+    return f"""[INST]{prompt}[/INST]
+"""
 
-### User:
-{prompt}
 
-### Assistant:"""
+# trim it down if too large
+STEP_CUT_TOKENS = 32
+
+# this is ugly, but basically we only have an approximation how many tokens
+# we are currently using. So we cannot just cut down to the desired size
+# what we're doing is:
+#   - take our current token count
+#   - use the minimum of (current_count, desired count *2)
+#     - this get's us roughly in the ballpark of the desired size
+#     - as long as we assume that 2 * desired-count will always be larger
+#       than the unschaerfe introduced by the string-.token conversion
+#   - do a 'binary search' to cut-down to the desired size afterwards
+#
+# this should reduce the time needed to do the string->token conversion
+# as this can be long-running if the LLM puts in a 'find /' output
+def trim_result_front(model, target_size, result):
+    cur_size = num_tokens_from_string(model, result)
+
+    TARGET_SIZE_FACTOR = 3
+    if cur_size > TARGET_SIZE_FACTOR * target_size:
+        print(f"big step trim-down from {cur_size} to {2*target_size}")
+        result = result[:TARGET_SIZE_FACTOR*target_size]
+        cur_size = num_tokens_from_string(model, result)
+   
+    while cur_size > target_size:
+        print(f"need to trim down from {cur_size} to {target_size}")
+        diff = cur_size - target_size
+        step = int((diff + STEP_CUT_TOKENS)/2)
+        result = result[:-step]
+        cur_size = num_tokens_from_string(model, result)
+
+    return result

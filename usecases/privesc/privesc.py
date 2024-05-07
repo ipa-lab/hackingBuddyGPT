fbe6ba7162242fbb2e69d266ce6a95672c57a0c3
Andreas Happe <andreashappe@snikt.net>
2024-05-07 15:16:11 +0200

    chg: move hints.json file-reading into a separate class
    This should be the first step towards multi-level LLM decision
    making, e.g., use-cases where we have an outer loop containing e.g.
    high-level decision making, and an inner loop that e.g. contains
    a concrete sub-usecase execution.
    
    For example: we have an outer loop that can iterate over multiple
    attack classes, and an inner loop that executes examples for the
    respective (current) attack class

--- usecases/privesc/privesc.py
+++ usecases/privesc/privesc.py
@@ -12,11 +12,71 @@ from utils import SSHConnection, PSExecConnection, llm_util, ui
 from usecases.usecase import use_case, UseCase
 from usecases.usecase.roundbased import RoundBasedUseCase
 from utils.cli_history import SlidingCliHistory
+from utils.console.console import Console
+from utils.db_storage.db_storage import DbStorage
+from utils.openai.openai_llm import OpenAIConnection
 
 template_dir = pathlib.Path(__file__).parent / "templates"
 template_next_cmd = Template(filename=str(template_dir / "query_next_command.txt"))
 template_analyze = Template(filename=str(template_dir / "analyze_cmd.txt"))
 template_state = Template(filename=str(template_dir / "update_state.txt"))
+
+@use_case("linux_privesc_hintfile", "Linux Privilege Escalation using a hints file")
+@dataclass
+class PrivescWithHintFile(UseCase, abc.ABC):
+    conn: SSHConnection = None
+    system: str = ''
+    enable_explanation: bool = False
+    enable_update_state: bool = False
+    disable_history: bool = False
+    hints: str = ""
+
+    # all of these would typically be set by RoundBasedUseCase :-/
+    # but we need them here so that we can pass them on to the inner
+    # use-case
+    log_db: DbStorage = None
+    console: Console = None
+    llm: OpenAIConnection = None
+    tag: str = ""
+    max_turns: int = 10
+
+    def init(self):
+        super().init()
+
+    # simple helper that reads the hints file and returns the hint
+    # for the current machine (test-case)
+    def read_hint(self):
+        if self.hints != "":
+            try:
+                with open(self.hints, "r") as hint_file:
+                    hints = json.load(hint_file)
+                    if self.conn.hostname in hints:
+                        return hints[self.conn.hostname]
+            except:
+                self.console.print("[yellow]Was not able to load hint file")
+        else:
+            self.console.print("[yellow]calling the hintfile use-case without a hint file?")
+        return ""
+
+    def run(self):
+        # read the hint
+        hint = self.read_hint()
+         
+        # call the inner use-case
+        priv_esc = LinuxPrivesc(
+            conn=self.conn, # must be set in sub classes
+            enable_explanation=self.enable_explanation,
+            disable_history=self.disable_history,
+            hint=hint,
+            log_db = self.log_db,
+            console = self.console,
+            llm = self.llm,
+            tag = self.tag,
+            max_turns = self.max_turns
+        )
+
+        priv_esc.init()
+        priv_esc.run()
 
 @dataclass
 class Privesc(RoundBasedUseCase, UseCase, abc.ABC):
@@ -25,26 +85,18 @@ class Privesc(RoundBasedUseCase, UseCase, abc.ABC):
     enable_explanation: bool = False
     enable_update_state: bool = False
     disable_history: bool = False
-    hints: str = ""
+    hint: str = ""
     
     _sliding_history: SlidingCliHistory = None
     _state: str = ""
-    _hint: str = None
     _capabilities: Dict[str, Capability] = field(default_factory=dict)
 
     def init(self):
         super().init()
 
     def setup(self):
-        if self.hints != "":
-            try:
-                with open(self.hints, "r") as hint_file:
-                    hints = json.load(hint_file)
-                    if self.conn.hostname in hints:
-                        self._hint = hints[self.conn.hostname]
-                        self.console.print(f"[bold green]Using the following hint: '{self._hint}'")
-            except:
-                self.console.print("[yellow]Was not able to load hint file")
+        if self.hint != "":
+            self.console.print(f"[bold green]Using the following hint: '{self.hint}'")
         
         if self.disable_history == False:
             self._sliding_history = SlidingCliHistory(self.llm)
@@ -107,7 +159,7 @@ class Privesc(RoundBasedUseCase, UseCase, abc.ABC):
         if not self.disable_history:
             history = self._sliding_history.get_history(self.llm.context_size - llm_util.SAFETY_MARGIN - state_size - template_size)
 
-        cmd = self.llm.get_response(template_next_cmd, _capabilities=self._capabilities, history=history, state=self._state, conn=self.conn, system=self.system, update_state=self.enable_update_state, target_user="root", hint=self._hint)
+        cmd = self.llm.get_response(template_next_cmd, _capabilities=self._capabilities, history=history, state=self._state, conn=self.conn, system=self.system, update_state=self.enable_update_state, target_user="root", hint=self.hint)
         cmd.result = llm_util.cmd_output_fixer(cmd.result)
         return cmd
 

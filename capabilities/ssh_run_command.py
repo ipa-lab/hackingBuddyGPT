d8791beb2a89fb6907759bba93e82bb8424b7a8c
Andreas Happe <andreashappe@snikt.net>
2024-05-07 19:06:38 +0200

    add a configurable timeout to ssh-runcommand

--- capabilities/ssh_run_command.py
+++ capabilities/ssh_run_command.py
@@ -22,7 +22,7 @@ class SSHRunCommand(Capability):
     def describe(self, name: str = None) -> str:
         return f"give a command to be executed on the shell and I will respond with the terminal output when running this command on the linux server. The given command must not require user interaction. Only state the to be executed command. The command should be used for enumeration or privilege escalation."
 
-    def __call__(self, command: str) -> Tuple[str, bool]:
+    def __call__(self, command: str, timeout:int=10) -> Tuple[str, bool]:
         got_root = False
         sudo_pass = Responder(
             pattern=r'\[sudo\] password for ' + self.conn.username + ':',
@@ -32,7 +32,7 @@ class SSHRunCommand(Capability):
         out = StringIO()
 
         try:
-            resp = self.conn.run(command, pty=True, warn=True, out_stream=out, watchers=[sudo_pass], timeout=10)
+            resp = self.conn.run(command, pty=True, warn=True, out_stream=out, watchers=[sudo_pass], timeout=timeout)
         except Exception as e:
             print("TIMEOUT! Could we have become root?")
         out.seek(0)

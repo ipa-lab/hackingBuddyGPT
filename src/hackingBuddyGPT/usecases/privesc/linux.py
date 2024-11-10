e0316524b674fa87e367559009cb2d8ba06595a4
Benjamin Probst <benjamin.probst@gmx.at>
2024-11-10 15:01:31 +0100

    added option to use hint file

--- src/hackingBuddyGPT/usecases/privesc/linux.py
+++ src/hackingBuddyGPT/usecases/privesc/linux.py
@@ -1,3 +1,5 @@
+import json
+
 from hackingBuddyGPT.capabilities import SSHRunCommand, SSHTestCredential
 from .common import Privesc, ThesisPrivescPrototyp
 from hackingBuddyGPT.utils import SSHConnection
@@ -31,4 +33,23 @@ class ThesisLinuxPrivescPrototyp(ThesisPrivescPrototyp):
 
 @use_case("Thesis Linux Privilege Escalation Prototyp")
 class ThesisLinuxPrivescPrototypUseCase(AutonomousAgentUseCase[ThesisLinuxPrivescPrototyp]):
-    pass
+    hints: str = ""
+
+    def init(self):
+        super().init()
+        if self.hints != "":
+            self.agent.hint = self.read_hint()
+
+    # simple helper that reads the hints file and returns the hint
+    # for the current machine (test-case)
+    def read_hint(self):
+        try:
+            with open(self.hints, "r") as hint_file:
+                hints = json.load(hint_file)
+                if self.agent.conn.hostname in hints:
+                    return hints[self.agent.conn.hostname]
+        except FileNotFoundError:
+            self._log.console.print("[yellow]Hint file not found")
+        except Exception as e:
+            self._log.console.print("[yellow]Hint file could not loaded:", str(e))
+        return ""

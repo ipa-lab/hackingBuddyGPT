117408ff59d439a1bae0bf9d95abf13976bbe444
lloydchang <lloydchang@gmail.com>
2024-11-13 02:19:01 -0800

    fix: use gemini-1.5-flash-latest

--- mac_start_hackingbuddygpt_against_a_container.sh
+++ mac_start_hackingbuddygpt_against_a_container.sh
@@ -53,4 +53,4 @@ PORT=$(docker ps | grep ansible-ready-ubuntu | cut -d ':' -f2 | cut -d '-' -f1)
 # Gemini free tier has a limit of 15 requests per minute, and 1500 requests per day
 # Hence --max_turns 999999999 will exceed the daily limit
 
-wintermute LinuxPrivesc --llm.api_key=$GEMINI_API_KEY --llm.model=gpt-4-turbo --llm.context_size=8192 --conn.host=localhost --conn.port $PORT --conn.username=lowpriv --conn.password=trustno1 --conn.hostname=test1 --llm.api_url=http://localhost:8080 --llm.api_backoff=60 --max_turns 999999999
+wintermute LinuxPrivesc --llm.api_key=$GEMINI_API_KEY --llm.model=gemini-1.5-flash-latest --llm.context_size=8192 --conn.host=localhost --conn.port $PORT --conn.username=lowpriv --conn.password=trustno1 --conn.hostname=test1 --llm.api_url=http://localhost:8080 --llm.api_backoff=60 --max_turns 999999999

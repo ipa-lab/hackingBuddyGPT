70f3f63d1edb2cef28e71b983d87120daeea0604
Benjamin Probst <benjamin.probst@gmx.at>
2024-11-17 14:51:01 +0100

    extended RAG database with hacktricks pages

--- src/hackingBuddyGPT/utils/rag_utility.py
+++ src/hackingBuddyGPT/utils/rag_utility.py
@@ -4,6 +4,8 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import MarkdownTextSplitter
+
 
 def initiate_rag():
     load_dotenv()
@@ -14,11 +16,19 @@ def initiate_rag():
 
     embeddings = OpenAIEmbeddings(model=os.environ['rag_embedding'], api_key=os.environ['openai_api_key'])
 
+    markdown_splitter = MarkdownTextSplitter(chunk_size=2000, chunk_overlap=0)
+
     if not os.path.exists(persistent_directory):
-        doc_manager = DocumentManager(os.path.join(rag_storage_path, "GTFObinMarkdown"))
-        doc_manager.load_documents()
+        doc_manager_1 = DocumentManager(os.path.join(rag_storage_path, "GTFObinMarkdown"))
+        doc_manager_1.load_documents()
+
+        doc_manager_2 = DocumentManager(os.path.join(rag_storage_path, "hacktricksMarkdownFiles"))
+        doc_manager_2.load_documents()
+        documents_hacktricks = markdown_splitter.split_documents(doc_manager_2.documents)
+
+        all_documents = doc_manager_1.documents + documents_hacktricks
         print(f"\n--- Creating vector store in {persistent_directory} ---")
-        db = Chroma.from_documents(doc_manager.documents, embeddings, persist_directory=persistent_directory)
+        db = Chroma.from_documents(all_documents, embeddings, persist_directory=persistent_directory)
         print(f"--- Finished creating vector store in {persistent_directory} ---")
     else:
         print(f"Vector store {persistent_directory} already exists. No need to initialize.")
@@ -26,7 +36,7 @@ def initiate_rag():
 
     retriever = db.as_retriever(
         search_type="similarity",
-        search_kwargs={"k": 3},
+        search_kwargs={"k": 10},
     )
 
     return retriever

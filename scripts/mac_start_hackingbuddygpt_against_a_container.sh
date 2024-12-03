0cea0d7b0d907172233138ee1b389741840ad06b
lloydchang <lloydchang@gmail.com>
2024-12-02 16:56:37 -0800

    fix: use gpt-4 which maps to gemini-1.5-flash-latest

--- scripts/mac_start_hackingbuddygpt_against_a_container.sh
+++ scripts/mac_start_hackingbuddygpt_against_a_container.sh
@@ -54,9 +54,23 @@ echo
 
 PORT=$(docker ps | grep ansible-ready-ubuntu | cut -d ':' -f2 | cut -d '-' -f1)
 
-# Gemini free tier has a limit of 15 requests per minute, and 1500 requests per day
-# Hence --max_turns 999999999 will exceed the daily limit
-
 # http://localhost:8080 is genmini-openai-proxy
 
-wintermute LinuxPrivesc --llm.api_key=$GEMINI_API_KEY --llm.model=gemini-1.5-flash-latest --llm.context_size=1000000 --conn.host=localhost --conn.port $PORT --conn.username=lowpriv --conn.password=trustno1 --conn.hostname=test1 --llm.api_url=http://localhost:8080 --llm.api_backoff=60 --max_turns 999999999
+# gpt-4 maps to gemini-1.5-flash-latest
+
+# https://github.com/zhu327/gemini-openai-proxy/blob/559085101f0ce5e8c98a94fb75fefd6c7a63d26d/README.md?plain=1#L146
+
+#    | gpt-4 | gemini-1.5-flash-latest |
+
+# https://github.com/zhu327/gemini-openai-proxy/blob/559085101f0ce5e8c98a94fb75fefd6c7a63d26d/pkg/adapter/models.go#L60-L61
+
+# 	case strings.HasPrefix(openAiModelName, openai.GPT4):
+# 		return Gemini1Dot5Flash
+
+# Hence use gpt-4 below in --llm.model=gpt-4
+
+# Gemini free tier has a limit of 15 requests per minute, and 1500 requests per day
+
+# Hence --max_turns 999999999 will exceed the daily limit
+
+wintermute LinuxPrivesc --llm.api_key=$GEMINI_API_KEY --llm.model=gpt-4 --llm.context_size=1000000 --conn.host=localhost --conn.port $PORT --conn.username=lowpriv --conn.password=trustno1 --conn.hostname=test1 --llm.api_url=http://localhost:8080 --llm.api_backoff=60 --max_turns 999999999

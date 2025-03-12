fffdaa461de8c9202bd4cb5779eead67e0f024e4
Jam Leomi <3192230+jamfish@users.noreply.github.com>
2025-03-12 16:45:28 -0500

    Update ssh_connection.py
    Adding keyfile parameter in order to use keys in operations.

--- src/hackingBuddyGPT/utils/ssh_connection/ssh_connection.py
+++ src/hackingBuddyGPT/utils/ssh_connection/ssh_connection.py
@@ -14,6 +14,7 @@ class SSHConnection:
     username: str
     password: str
     port: int = 22
+    keyfilename: str
 
     _conn: Connection = None
 
@@ -21,18 +22,19 @@ class SSHConnection:
         # create the SSH Connection
         conn = Connection(
             f"{self.username}@{self.host}:{self.port}",
-            connect_kwargs={"password": self.password, "key_filename": "/home/myuser/.ssh/private.key", "allow_agent": True},
+            connect_kwargs={"password": self.password, "key_filename": self.keyfilename , "allow_agent": True},
         )
         self._conn = conn
         self._conn.open()
 
-    def new_with(self, *, host=None, hostname=None, username=None, password=None, port=None) -> "SSHConnection":
+    def new_with(self, *, host=None, hostname=None, username=None, password=None, port=None, keyfilename=None) -> "SSHConnection":
         return SSHConnection(
             host=host or self.host,
             hostname=hostname or self.hostname,
             username=username or self.username,
             password=password or self.password,
             port=port or self.port,
+            keyfilename=keyfilename or self.keyfilename,
         )
 
     def run(self, cmd, *args, **kwargs) -> Tuple[str, str, int]:

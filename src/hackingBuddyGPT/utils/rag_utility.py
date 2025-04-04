07951b6adab5e035c40623841db6069a22392c7b
Benjamin Probst <benjamin.probst@gmx.at>
2025-04-04 12:40:20 +0200

    updated dependencies

--- src/hackingBuddyGPT/utils/rag_utility.py
+++ src/hackingBuddyGPT/utils/rag_utility.py
@@ -2,7 +2,7 @@ import os
 
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from dotenv import load_dotenv
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import MarkdownTextSplitter
 

9edd714f9b7ff6c6df7dcef71d188f3483053a1a
Benjamin Probst <benjamin.probst@gmx.at>
2025-04-08 13:55:05 +0200

    removed parameter keyfilename from __call__

--- src/hackingBuddyGPT/capabilities/ssh_test_credential.py
+++ src/hackingBuddyGPT/capabilities/ssh_test_credential.py
@@ -18,8 +18,8 @@ class SSHTestCredential(Capability):
     def get_name(self):
         return "test_credential"
 
-    def __call__(self, username: str, password: str, keyfilename: str) -> Tuple[str, bool]:
-        test_conn = self.conn.new_with(username=username, password=password, keyfilename=keyfilename)
+    def __call__(self, username: str, password: str) -> Tuple[str, bool]:
+        test_conn = self.conn.new_with(username=username, password=password)
         try:
             test_conn.init()
             user = test_conn.run("whoami")[0].strip("\n\r ")

dd79f4bef6f71a1ea91230128217dc483f086ada
Benjamin Probst <benjamin.probst@gmx.at>
2025-04-10 11:53:25 +0200

    added Chain of thought component

--- src/hackingBuddyGPT/usecases/rag/common.py
+++ src/hackingBuddyGPT/usecases/rag/common.py
@@ -1,5 +1,7 @@
 import datetime
 import pathlib
+import re
+
 from dataclasses import dataclass, field
 from mako.template import Template
 from typing import Any, Dict, Optional
@@ -14,6 +16,7 @@ from hackingBuddyGPT.utils.cli_history import SlidingCliHistory
 template_dir = pathlib.Path(__file__).parent / "templates"
 template_next_cmd = Template(filename=str(template_dir / "query_next_command.txt"))
 template_analyze = Template(filename=str(template_dir / "analyze_cmd.txt"))
+template_chain_of_thought = Template(filename=str(template_dir / "chain_of_thought.txt"))
 
 
 @dataclass
@@ -23,6 +26,7 @@ class ThesisPrivescPrototype(Agent):
     enable_update_state: bool = False
     enable_compressed_history: bool = False
     disable_history: bool = False
+    enable_chain_of_thought: bool = False
     hint: str = ""
 
     _sliding_history: SlidingCliHistory = None
@@ -45,7 +49,11 @@ class ThesisPrivescPrototype(Agent):
             "conn": self.conn,
             "update_state": self.enable_update_state,
             "target_user": "root",
+            'chain_of_thought': self.enable_chain_of_thought
         }
+
+        if self.enable_chain_of_thought:
+            self._chain_of_thought = template_chain_of_thought.source
 
         template_size = self.llm.count_tokens(template_next_cmd.source)
         self._max_history_size = self.llm.context_size - llm_util.SAFETY_MARGIN - template_size
@@ -53,7 +61,21 @@ class ThesisPrivescPrototype(Agent):
     def perform_round(self, turn: int) -> bool:
         # get the next command and run it
         cmd, message_id = self.get_next_command()
-        result, got_root = self.run_command(cmd, message_id)
+
+
+        if self.enable_chain_of_thought:
+            # command = re.findall("<command>(.*?)</command>", answer.result)
+            command = re.findall(r"<command>([\s\S]*?)</command>", cmd)
+
+            if len(command) > 0:
+                command = "\n".join(command)
+                cmd = command
+
+        # split if there are multiple commands
+        commands = self.split_into_multiple_commands(cmd)
+
+        cmds, result, got_root = self.run_command(commands, message_id)
+
 
         # log and output the command and its result
         if self._sliding_history:
@@ -72,37 +94,54 @@ class ThesisPrivescPrototype(Agent):
         # if we got root, we can stop the loop
         return got_root
 
+    def get_chain_of_thought_size(self) -> int:
+        if self.enable_chain_of_thought:
+            return self.llm.count_tokens(self._chain_of_thought)
+        else:
+            return 0
+
     @log_conversation("Asking LLM for a new command...", start_section=True)
     def get_next_command(self) -> tuple[str, int]:
         history = ""
         if not self.disable_history:
             if self.enable_compressed_history:
-                history = self._sliding_history.get_commands_and_last_output(self._max_history_size)
+                history = self._sliding_history.get_commands_and_last_output(self._max_history_size - self.get_chain_of_thought_size())
             else:
-                history = self._sliding_history.get_history(self._max_history_size)
+                history = self._sliding_history.get_history(self._max_history_size - self.get_chain_of_thought_size())
 
-        self._template_params.update({"history": history, "state": self._state})
+        self._template_params.update({"history": history, 'CoT': self._chain_of_thought})
 
         cmd = self.llm.get_response(template_next_cmd, **self._template_params)
         message_id = self.log.call_response(cmd)
 
-        return llm_util.cmd_output_fixer(cmd.result), message_id
+        # return llm_util.cmd_output_fixer(cmd.result), message_id
+        return cmd.result, message_id
 
     @log_section("Executing that command...")
-    def run_command(self, cmd, message_id) -> tuple[Optional[str], bool]:
+    def run_command(self, cmd, message_id) -> tuple[Optional[str], Optional[str], bool]:
         _capability_descriptions, parser = capabilities_to_simple_text_handler(self._capabilities, default_capability=self._default_capability)
-        start_time = datetime.datetime.now()
-        success, *output = parser(cmd)
-        if not success:
-            self.log.add_tool_call(message_id, tool_call_id=0, function_name="", arguments=cmd, result_text=output[0], duration=0)
-            return output[0], False
 
-        assert len(output) == 1
-        capability, cmd, (result, got_root) = output[0]
-        duration = datetime.datetime.now() - start_time
-        self.log.add_tool_call(message_id, tool_call_id=0, function_name=capability, arguments=cmd, result_text=result, duration=duration)
+        cmds = ""
+        result = ""
+        got_root = False
+        for i, command in enumerate(cmd):
+            start_time = datetime.datetime.now()
+            success, *output = parser(command)
+            if not success:
+                self.log.add_tool_call(message_id, tool_call_id=0, function_name="", arguments=command, result_text=output[0], duration=0)
+                return cmds, output[0], False
+            assert len(output) == 1
+            capability, cmd_, (result_, got_root_) = output[0]
+            cmds += cmd_ + "\n"
+            result += result_ + "\n"
+            got_root = got_root or got_root_
+            duration = datetime.datetime.now() - start_time
+            self.log.add_tool_call(message_id, tool_call_id=i, function_name=capability, arguments=cmd_,
+                                   result_text=result_, duration=duration)
 
-        return result, got_root
+        cmds = cmds.rstrip()
+        result = result.rstrip()
+        return cmds, result, got_root
 
     @log_conversation("Analyze its result...", start_section=True)
     def analyze_result(self, cmd, result):
@@ -113,3 +152,24 @@ class ThesisPrivescPrototype(Agent):
         result = llm_util.trim_result_front(self.llm, target_size, result)
         answer = self.llm.get_response(template_analyze, cmd=cmd, resp=result, facts=self._state)
         self.log.call_response(answer)
+
+    def split_into_multiple_commands(self, response: str):
+        ret = self.split_with_delimiters(response, ["test_credential", "exec_command"])
+
+        # strip trailing newlines
+        ret = [r.rstrip() for r in ret]
+
+        # remove first entry. For some reason its always empty
+        if len(ret) > 1:
+            ret = ret[1:]
+
+        # combine keywords with their corresponding input
+        if len(ret) > 1:
+            ret = [ret[i] + ret[i + 1] for i in range(0, len(ret) - 1, 2)]
+        return ret
+
+    def split_with_delimiters(self, input: str, delimiters):
+        # Create a regex pattern to match any of the delimiters
+        regex_pattern = f"({'|'.join(map(re.escape, delimiters))})"
+        # Use re.split to split the text while keeping the delimiters
+        return re.split(regex_pattern, input)

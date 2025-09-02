c690b767c17b9b24cc71447bab18f4260ae5752b
Andreas Happe <andreashappe@snikt.net>
2025-09-02 14:41:01 +0200

    remove old-style linux-privesc and parts of examples

--- src/hackingBuddyGPT/usecases/__init__.py
+++ src/hackingBuddyGPT/usecases/__init__.py
@@ -1,5 +1,3 @@
-from .examples import *
-from .privesc import *
 from .web import *
 from .web_api_testing import *
 from .viewer import *
--- src/hackingBuddyGPT/usecases/examples/__init__.py
+++
@@ -1,4 +0,0 @@
-from .agent import ExPrivEscLinux
-from .agent_with_state import ExPrivEscLinuxTemplated
-from .hintfile import ExPrivEscLinuxHintFileUseCase
-from .lse import ExPrivEscLinuxLSEUseCase

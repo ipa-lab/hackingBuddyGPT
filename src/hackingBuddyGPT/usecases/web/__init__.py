91f70e3d05dfe15e27d0f35685c7853e48022307
Neverbolt <manuel@reinsperger.org>
2025-11-24 22:25:32 +0100

    Adds WebTestingWithShell

--- src/hackingBuddyGPT/usecases/web/__init__.py
+++ src/hackingBuddyGPT/usecases/web/__init__.py
@@ -1,4 +1,5 @@
-from .with_explanation import WebTestingWithExplanation
 from .advanced import AdvancedWebTesting
+from .with_explanation import WebTestingWithExplanation
+from .with_shell import WebTestingWithShell
 
-__all__ = ["WebTestingWithExplanation", "AdvancedWebTesting"]
+__all__ = ["WebTestingWithExplanation", "AdvancedWebTesting", "WebTestingWithShell"]
